fed6c76c5714e962d6a240cf40c88fc0ef544db3
thomas grice <augustus.grice@gmail.com>
2016-04-13 13:57:12 -0500

    nbl implement

--- NBL.py
+++ NBL.py
@@ -1,6 +1,21 @@
 # Naive Bayes Learner class
 
 class NBL:
+    priorProbabilities = {} # probability of class being 1 given an attribute being 1
+    classProbabilities = {}
+    attributes = []
 
-    def __init__(self):
-        pass
+    def __init__(self, trainingdata, attributes):
+        self.calculateProbabilities(trainingdata)
+
+    # calculate probabilities based on training data
+    def calculateProbabilities(self, trainingdata):
+        attrInfo = {}
+        for attribute in self.attributes:
+            attrInfo[attribute] = {
+                "totalPositive":0,
+                "totalNegative":0,
+                "class1WhenPositive":0,
+                "class1WhenNegative":0
+            }
+
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import util
 import sys
+import NBL
 
 args = sys.argv
 #arg[1] is training data
 #arg[2] is test data
 attributes, trainingData = util.readFile(args[1])
+
+naivebayeslearner = NBL.NBL(trainingData, attributes)
+
+
 testAttributes, testData = util.readFile(args[2])
